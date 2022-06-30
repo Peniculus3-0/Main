62761ba3e226dfc27eeef526d561cedf17b184a4
ecp68 <etienne.cpoitevin@gmail.com>
2022-06-30 11:53:15 -0400

    remove ninja

--- Pendulus/src/main.cpp
+++ Pendulus/src/main.cpp
@@ -5,9 +5,6 @@
  * date: Juin 2022
 */
 
-
-//Je suis un ninja
-
 /*------------------------------ Librairies ---------------------------------*/
 #include <ArduinoJson.h> // librairie de syntaxe JSON
 #include <SPI.h> // librairie Communication SPI
